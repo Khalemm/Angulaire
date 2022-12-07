6f8c63c042fac4be5be547eec47527e186125358
Luca MARTINOLLI <martinolli.luca@gmail.com>
2022-12-07 18:32:08 +0100

    création architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/shared/header/header.component';
+import { ItemCardComponent } from './component/item/item-card/item-card.component';
+import { ItemInfoComponent } from './component/item/item-info/item-info.component';
+import { MainPageComponent } from './component/shared/main-page/main-page.component';
+import { LoginComponent } from './component/user/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ItemCardComponent,
+    ItemInfoComponent,
+    MainPageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
